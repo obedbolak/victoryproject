74401864421d354686a5ab73d2f37907e2ae0789
obedbolak <jamesnormankate@gmail.com>
2026-02-23 14:42:14 +0100

    update

--- withVpnLib.js
+++ withVpnLib.js
@@ -3,10 +3,8 @@ const { withSettingsGradle } = require('@expo/config-plugins');
 module.exports = function withVpnLib(config) {
   return withSettingsGradle(config, (config) => {
     const contents = config.modResults.contents;
-    const include = `
-include ':vpnLib'
-project(':vpnLib').projectDir = new File(rootProject.projectDir, '../node_modules/react-native-simple-openvpn/vpnLib')
-`;
+    const include = "\ninclude ':vpnLib'\nproject(':vpnLib').projectDir = new File(rootProject.projectDir, '../node_modules/react-native-simple-openvpn/vpnLib')\n";
+    if (contents.indexOf(':vpnLib') === -1) {
       config.modResults.contents = contents + include;
     }
     return config;
